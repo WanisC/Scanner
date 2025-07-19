3d7e7a246bcee502b9cbcbdbb4ed15b35c76e0b8
WanisC <wanischouaib1@gmail.com>
2025-07-19 19:11:31 +0200

    Adding log option/Setting up for IPv6

--- src/main.rs
+++ src/main.rs
@@ -36,14 +36,14 @@ enum Commands {
     ScanIpv4 {
         #[clap(long_help = "IPv4 address", required = true)]
         ipv4_adr: String,
+
+        #[clap(long_help = "Create JSON file", required = true)]
+        log: String,
     },
 
     ScanIpv6 {
-        #[clap(long_help = "Fist pair of bytes", required = true)]
-        octet1: u8,
-
-        #[clap(long_help = "Second pair of bytes", required = true)]
-        octet2: u8,
+         #[clap(long_help = "IPv4 address", required = true)]
+        ipv4_adr: String,
     },
 
     ScanNet {},
@@ -75,7 +75,7 @@ async fn main() -> std::io::Result<()> {
             }
         },
 
-        Commands::ScanIpv4 { ipv4_adr } => {
+        Commands::ScanIpv4 { ipv4_adr, log } => {
             println!("[-] Local network scan of {:?}", ipv4_adr);
             let octets_result: Result<Vec<u8>, _> = ipv4_adr
                 .split('.')
@@ -88,13 +88,23 @@ async fn main() -> std::io::Result<()> {
                     std::process::exit(1);
                 }
             };
-            scan_ipv4::ipv4(&octets);
-            println!("[-] Results are stored in a JSON file");
+            scan_ipv4::ipv4(&octets, &log);
         },
 
-        Commands::ScanIpv6 { octet1, octet2 } => {
-            println!("Local network scan of... with octet1={}, octet2={} for IPv6", octet1, octet2);
-            scan_ipv6::ipv6(*octet1, *octet2);
+        Commands::ScanIpv6 { ipv4_adr } => {
+            println!("[-] Local network scan of {:?}", ipv4_adr);
+            let octets_result: Result<Vec<u8>, _> = ipv4_adr
+                .split('.')
+                .map(|s| s.parse::<u8>())
+                .collect();
+            let octets = match octets_result {
+                Ok(octs) if (1..=4).contains(&octs.len()) => octs,
+                _ => {
+                    eprintln!("Invalid IP address, must be 1 to 4 bytes between 0 and 255");
+                    std::process::exit(1);
+                }
+            };
+            scan_ipv6::ipv6(&octets);
         },
 
         Commands::ScanNet {} => {},
